b98910ff8ab33cd97a96af9d748ad9728389800d
Torrez, Milton N <torrez.mn@gmail.com>
2021-07-17 02:28:00 +0200

    New delivery modal added.

--- component/script.js
+++ component/script.js
@@ -145,6 +145,43 @@ class Desk extends HTMLElement {
                         margin-top:5px;
                         cursor:pointer;
                     }
+                    /* The Modal (background) */
+                    .modal {
+                      display: none; /* Hidden by default */
+                      position: fixed; /* Stay in place */
+                      z-index: 1; /* Sit on top */
+                      left: 0;
+                      top: 0;
+                      width: 100%; /* Full width */
+                      height: 100%; /* Full height */
+                      overflow: auto; /* Enable scroll if needed */
+                      background-color: rgb(0,0,0); /* Fallback color */
+                      background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+                    }
+                    
+                    /* Modal Content/Box */
+                    .modal-content {
+                      background-color: #fefefe;
+                      margin: 15% auto; /* 15% from the top and centered */
+                      padding: 20px;
+                      border: 1px solid #888;
+                      width: 80%; /* Could be more or less, depending on screen size */
+                    }
+                    
+                    /* The Close Button */
+                    .close {
+                      color: #aaa;
+                      float: right;
+                      font-size: 28px;
+                      font-weight: bold;
+                    }
+                    
+                    .close:hover,
+                    .close:focus {
+                      color: black;
+                      text-decoration: none;
+                      cursor: pointer;
+                    } 
                     </style>
                     <div class="desk">
                         <div class="desk_botonera">
@@ -162,6 +199,18 @@ class Desk extends HTMLElement {
                         </div>
                         
                     </div>
+
+
+                    <!-- The Modal -->
+<div id="nuevo_repartidor" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <p>Some text in the Modal..</p>
+  </div>
+
+</div>
 
                 `;
         this.attachShadow({ mode: 'open' });
@@ -196,7 +245,17 @@ class Desk extends HTMLElement {
         this.shadowRoot.querySelector('.add_employee').addEventListener('click', (ev) => {
             this.delivery_config.employees.push(`<deliery-employee></deliery-employee>`);
             this.setAttribute('employees', this.delivery_config.employees.length);
-                      
+            
+            
+            let modal = this.shadowRoot.querySelector('#nuevo_repartidor');
+            
+            // Get the <span> element that closes the modal
+            let span = this.shadowRoot.querySelector(".close");
+            
+            modal.style.display = "block";
+            span.addEventListener('click', (ev)=>{
+                modal.style.display = "none";
+            })
             
             
             
